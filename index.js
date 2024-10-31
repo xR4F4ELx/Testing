01da39ed3cf6343f40e58e245c2c3712a073a762
xR4F4ELx <rafael_macam@yahoo.com>
2024-11-01 00:47:58 +0800

    uwed

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
 
-http.createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World!');
-}).listen(port);
+const server = http.createServer(function(request, response) { 
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end("<html><body><h1>Hello World!</h1></body></html>");
+});
+
+const port = process.env.PORT || 1337;
+server.listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
